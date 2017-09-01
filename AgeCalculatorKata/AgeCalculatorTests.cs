65cd9293619bfc63aa1952c51ac73ce93e605a00
T-rav <tmfrisinger@gmail.com>
2017-09-01 14:02:29 +0200

    First of 3 test to triangulate on the year portion of the algorithm.

--- AgeCalculatorKata/AgeCalculatorTests.cs
+++ AgeCalculatorKata/AgeCalculatorTests.cs
@@ -19,12 +19,28 @@ namespace Age.Calculator.Tests
             //---------------Assert -----------------------
             Assert.AreEqual(expected, result);
         }
+
+        [TestCase("01,01,2016","01,01,2017")]
+        public void Calculate_GivenBirthdayOneYearAgo_ShouldReturnOne(DateTime birthday, DateTime today)
+        {
+            //---------------Arrange-------------------
+            var expected = 1;
+            var ageCalculator = new AgeCalculator();
+            //---------------Act ----------------------
+            var result = ageCalculator.GetAge(birthday, today);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, result);
+        }
     }
 
     public class AgeCalculator
     {
         public int GetAge(DateTime birthDateTime, DateTime today)
         {
+            if (today != birthDateTime)
+            {
+                return 1;
+            }
             return 0;
         }
     }
