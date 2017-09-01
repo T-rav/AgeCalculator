c0a47d64a019f171d55975d93535cd5dc972160f
T-rav <tmfrisinger@gmail.com>
2017-09-01 14:00:51 +0200

    Got first test passing.

---
+++ AgeCalculatorKata/AgeCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Age.Calculator.Tests
+{
+    [TestFixture]
+    public class AgeCalculatorTests
+    {
+        [Test]
+        public void Calculate_GivenBirthdayToday_ShouldReturnZero()
+        {
+            //---------------Arrange-------------------
+            var birthday = DateTime.Now;
+            var today = DateTime.Now;
+            var expected = 0;
+            var ageCalculator = new AgeCalculator();
+            //---------------Act ----------------------
+            var result = ageCalculator.GetAge(birthday, today);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    public class AgeCalculator
+    {
+        public int GetAge(DateTime birthDateTime, DateTime today)
+        {
+            return 0;
+        }
+    }
+}
