03c06fedf8d3445a57b2db87304fe7148588262e
T-rav <tmfrisinger@gmail.com>
2017-09-05 07:54:06 +0200

    Reactored to remove the need for || in the age calculator

---
+++ Age.Calculator/AgeCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Age.Calculator
+{
+    public class AgeCalculator
+    {
+        public int GetAge(DateTime birthdate, DateTime today)
+        {
+            var age = CalculateGrossAge(birthdate, today);
+            if (BirthdayHasNotHappenedYet(birthdate, today, age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        private static bool BirthdayHasNotHappenedYet(DateTime birthdate, DateTime today, int age)
+        {
+            var negativeAge = -1 * age;
+            var canidateDate = today.AddYears(negativeAge);
+            return canidateDate.CompareTo(birthdate) != 0;
+        }
+
+        private static int CalculateGrossAge(DateTime birthdate, DateTime today)
+        {
+            return today.Year - birthdate.Year;
+        }
+    }
+}
